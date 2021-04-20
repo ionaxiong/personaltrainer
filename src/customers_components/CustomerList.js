8ff35046429e4ae7fe517cdc6634a3aa3fa77d83
ming xiong <ming.xiong@myy.haaga-helia.fi>
2021-04-20 23:10:57 +0300

    persistent drawer added in app.js

--- src/customers_components/CustomerList.js
+++ src/customers_components/CustomerList.js
@@ -1,46 +1,23 @@
 import React, { useState, useEffect } from "react";
 
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
-
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-
 function CustomerList() {
-    const [customers, setCustomers] = useState([]);
+  const [customers, setCustomers] = useState([]);
 
-    useEffect(() => {
-        fetchCustomers();
-    }, []);
+  const fetchCustomers = () => {
+    fetch("https://customerrest.herokuapp.com/api/customers")
+      .then((response) => response.json())
+      .then((data) => setCustomers(data.content))
+      .catch((err) => console.log(err));
+  };
 
-    const fetchCustomers = () => {
-        fetch('https://customerrest.herokuapp.com/api/customers')
-        .then(response => response.json())
-        .then(data => setCustomers(data.content))
-        .catch(err => console.log(err));
-    };
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
 
   return (
-    <div >
-        Customer List
-    </div>
+    <div>
+      Customer List
+      </div>
     );
 }
 
