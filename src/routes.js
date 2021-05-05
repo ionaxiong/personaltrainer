9221c181d78a91c3eb687359b884e8ab09063f3d
ming xiong <ming.xiong@myy.haaga-helia.fi>
2021-05-05 19:58:12 +0300

    try to implement routers for side bar pages

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,23 @@
+import React from 'react';
+import { Calendar } from 'react-feather';
 import { Navigate } from 'react-router-dom';
-import DashboardLayout from 'src/components/DashboardLayout';
-import MainLayout from 'src/components/MainLayout';
+import DashboardLayout from './components/DashboardLayout';
 import CustomerList from 'src/pages/CustomerList';
-import Dashboard from 'src/pages/Dashboard';
-import Login from 'src/pages/Login';
 import NotFound from 'src/pages/NotFound';
 import ProductList from 'src/pages/ProductList';
 import Register from 'src/pages/Register';
 import Settings from 'src/pages/Settings';
+import Customers from './pages/Customers';
 
 const routes = [
   {
     path: 'app',
     element: <DashboardLayout />,
     children: [
-      { path: 'customers', element: <CustomerList /> },
-      { path: 'dashboard', element: <Dashboard /> },
-      { path: 'products', element: <ProductList /> },
-      { path: 'settings', element: <Settings /> },
-      { path: '*', element: <Navigate to="/404" /> }
-    ]
-  },
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      { path: 'login', element: <Login /> },
-      { path: 'register', element: <Register /> },
-      { path: '404', element: <NotFound /> },
-      { path: '/', element: <Navigate to="/app/dashboard" /> },
-      { path: '*', element: <Navigate to="/404" /> }
+      { path: 'customers', element: <Customers /> },
+      { path: 'trainings', element: <Trainings /> },
+      { path: 'calendar', element: <Calendar /> },
+      { path: 'statistics', element: <Statistics /> },
     ]
   }
 ];
